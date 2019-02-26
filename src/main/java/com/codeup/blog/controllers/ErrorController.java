214e084d83528a8a98b684cb665e0c5308db905e
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-02-26 12:18:10 -0600

    ErrorController handles 404's generally

--- src/main/java/com/codeup/blog/controllers/ErrorController.java
+++ src/main/java/com/codeup/blog/controllers/ErrorController.java
@@ -1,12 +1,14 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ErrorController {
 
-    @GetMapping("/404")
+    //@GetMapping("/404")
+    @RequestMapping(value = "404Error", method = RequestMethod.GET)
     public String notFound(){
         // DEBUG
         System.out.println("DEBUG: ErrorController\n"
