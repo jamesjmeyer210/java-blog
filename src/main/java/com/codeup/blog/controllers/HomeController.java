6c490d5a5f6ff1d51e5cd185a3633fcbebacbd19
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-02-26 11:20:06 -0600

    Made changes to produce a successful build

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -2,14 +2,17 @@ package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index(){
-        return "home";
+        // DEBUG
+        System.out.println("DEBUG: HomeController\n"
+            + "\tindex()");
+        // END DEBUG
+        return "index";
     }
 
 }
